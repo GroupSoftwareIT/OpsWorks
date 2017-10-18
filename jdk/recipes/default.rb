c82e095dcebf3e7e856abd2054584fd6f03ca7b9
Luiz A. Amelotti <luiz.amelotti@groupsoftware.com.br>
2017-10-18 15:30:25 -0200

    Corrigindo processo de download do JDK

--- jdk/recipes/default.rb
+++ jdk/recipes/default.rb
@@ -8,26 +8,26 @@
 tmp_path = Chef::Config[:file_cache_path]
 
 #Download jdk archive
-#remote_file "#{tmp_path}/jdk.tar.gz" do
-#  source node['jdk']['download_url']
-#  owner node['jdk']['jdk_user']
-#  mode '0644'
-#  action :create
-#end
+remote_file "#{tmp_path}/jdk.tar.gz" do
+  source node['jdk']['download_url']
+  owner node['jdk']['jdk_user']
+  mode '0644'
+  action :create
+end
 
 #Download JDK archive from Oracle website
-bash 'Download jdk archive' do
-  user node['jdk']['jdk_user']
-  cwd node['jdk']['install_location']
-  code <<-EOH
-    wget --no-cookies \
---no-check-certificate \
---header "Cookie: oraclelicense=accept-securebackup-cookie" \
-"node['jdk']['download_url']" \
--O #{tmp_path}/jdk.tar.gz
-  EOH
-  action :run
-end
+#bash 'Download jdk archive' do
+#  user node['jdk']['jdk_user']
+#  cwd node['jdk']['install_location']
+#  code <<-EOH
+#    wget --no-cookies \
+#--no-check-certificate \
+#--header "Cookie: oraclelicense=accept-securebackup-cookie" \
+#"node['jdk']['download_url']" \
+#-O #{tmp_path}/jdk.tar.gz
+#  EOH
+#  action :run
+#end
 
 #create tomcat install dir
 directory node['jdk']['install_location'] do
---
+++ victor/README.md
@@ -0,0 +1,4 @@
+# victor
+
+TODO: Enter the cookbook description here.
+
