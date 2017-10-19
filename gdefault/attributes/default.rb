d1c0743e21cbd74362d09dd422f7f9e0f20bda81
Luiz A. Amelotti <luiz.amelotti@groupsoftware.com.br>
2017-10-19 13:49:34 -0200

    Ajustes e configuracao do S3FS

---
+++ gdefault/attributes/default.rb
@@ -0,0 +1,2 @@
+default['gdefault']['ak'] = "AKIAJPBS4QIXE27VEJXA"
+default['gdefault']['sk'] = "/vgE8ZmOIysnNTvncvFa6RJmahcMhXPUhT0f5xD4"
--- gdefault/recipes/default.rb
+++ gdefault/recipes/default.rb
@@ -6,11 +6,12 @@
 
 #Instalar ferramentas de apoio
 package ['htop', 'iftop', 'iotop', 'iptables-persistent', 'unzip', 'mysql-client']
+package ['automake', 'autotools-dev', 'g++', 'libcurl4-gnutls-dev', 'libfuse-dev', 'libssl-dev', 'libxml2-dev', 'make', 'pkg-config']
 
 
 #Configurar o timezone
 bash 'timezone' do
-  user root
+  user 'root'
   code <<-EOH
     echo "America/Sao_Paulo" > /etc/timezone
     dpkg-reconfigure --frontend noninteractive tzdata
@@ -30,11 +31,11 @@ end
 #Definir configurações do iptables
 template "/opt/iptables.rules" do
   source 'iptables-save.erb'
-  owner root
+  owner 'root'
   mode '0644'
 end
 bash 'iptables restore' do
-  user root
+  user 'root'
   code <<-EOH
     iptables-restore < /opt/iptables.rules
   EOH
@@ -62,4 +63,31 @@ directory '/opt/comunidades21' do
   group 'tomcat'
   mode '0770'
   action :create
+end
+
+
+#Instalação do s3fs
+remote_file "#{tmp_path}/s3fs.tgz" do
+  source 'https://s3.amazonaws.com/gopswordksdeps/s3fs-fuse.tgz'
+  owner 'root'
+  mode '0644'
+  action :create
+end
+template "/root/.passwd-s3fs" do
+  source 's3pass.erb'
+  owner 'root'
+  mode '0600'
+end
+bash 'Extract s3fs archive' do
+  user 'root'
+  cwd '/opt/'
+  code <<-EOH
+    tar -zxvf #{tmp_path}/s3fs.tgz --strip 1
+	cd s3fs-fuse
+	./configure
+	make clean && make
+	make install
+	s3fs -o allow_other -o passwd_file=/root/.passwd-s3fs comunidade21.config /comunidade21/
+  EOH
+  action :run
 end
