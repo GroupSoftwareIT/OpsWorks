ab3f00b508d70ed2f885a7c0b9583e7dd24849db
Luiz A. Amelotti <luiz.amelotti@groupsoftware.com.br>
2017-10-24 15:14:23 -0200

    Ajustes deploy app

--- tomcat8/recipes/deploy.rb
+++ tomcat8/recipes/deploy.rb
@@ -1,7 +1,8 @@
 #app = search("aws_opsworks_app").first
 
 remote_file "node['tomcat8']['deploy_location']/node[:deploy]['shortname'].war" do
-  source node[:deploy]['appsource']['url']
+#  source node[:deploy]['appsource']['url']
+  source 'https://s3.amazonaws.com/gopswordksdeps/calendar.war'
   owner 'root'
   mode '0644'
   action :create
