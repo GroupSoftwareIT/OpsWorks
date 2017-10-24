ce9be4a98a360a06be8fcf10c23f58e4a1a41280
Luiz A. Amelotti <luiz.amelotti@groupsoftware.com.br>
2017-10-24 14:28:02 -0200

    Ajuste deploy aplicacao

--- tomcat8/recipes/deploy.rb
+++ tomcat8/recipes/deploy.rb
@@ -1,5 +1,5 @@
 remote_file "node['tomcat8']['deploy_location']/#{app['shortname']}.war" do
-  source '#{app['app_source']['url']}'
+  source #{app['app_source']['url']}
   owner 'root'
   mode '0644'
   action :create
