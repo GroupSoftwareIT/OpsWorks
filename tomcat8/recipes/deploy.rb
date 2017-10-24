f5e38e529341c9d3b2c6291c52856576250f3ad8
Luiz A. Amelotti <luiz.amelotti@groupsoftware.com.br>
2017-10-24 14:54:15 -0200

    Ajuste deploy app

--- tomcat8/recipes/deploy.rb
+++ tomcat8/recipes/deploy.rb
@@ -1,8 +1,10 @@
 #app = search("aws_opsworks_app").first
 
 remote_file "node['tomcat8']['deploy_location']/node[:deploy]['shortname'].war" do
-  source node[:deploy]['appshortname']['scm']['repository']
+  source node['deploy']['appshortname']['scm']['repository']
   owner 'root'
   mode '0644'
   action :create
 end
+
+
