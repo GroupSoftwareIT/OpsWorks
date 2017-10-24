57d2aecd7a4765da715827ca6dcbb2993db0d5cd
Luiz A. Amelotti <luiz.amelotti@groupsoftware.com.br>
2017-10-24 15:43:51 -0200

    Teste deploy app

--- tomcat8/recipes/deploy.rb
+++ tomcat8/recipes/deploy.rb
@@ -1,11 +1,14 @@
-app = search("aws_opsworks_app": "shortname:calendar").first
+app = search("aws_opsworks_app").first
 
-remote_file "/opt/tomcat8/webapps/calendar.war" do
-#  source node[:deploy]['appsource']['url']
-  source #{app['app_source']['url']}
-  owner 'root'
-  mode '0644'
-  action :create
-end
+Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+
+#remote_file "/opt/tomcat8/webapps/calendar.war" do
+##  source node[:deploy]['appsource']['url']
+#  source #{app['app_source']['url']}
+#  owner 'root'
+#  mode '0644'
+#  action :create
+#end
 
 
