6295419e654803b9f31bcbed005fc122336bb997
Luiz A. Amelotti <luiz.amelotti@groupsoftware.com.br>
2017-10-24 14:35:11 -0200

    Ajustes deploy app

--- tomcat8/recipes/deploy.rb
+++ tomcat8/recipes/deploy.rb
@@ -1,5 +1,7 @@
-remote_file "node['tomcat8']['deploy_location']/#{app['shortname']}.war" do
-  source #{app['app_source']['url']}
+#app = search("aws_opsworks_app").first
+
+remote_file "node['tomcat8']['deploy_location']/#{node[:deploy]['shortname']}.war" do
+  source #{node[:deploy]['app_source']['url']}
   owner 'root'
   mode '0644'
   action :create
