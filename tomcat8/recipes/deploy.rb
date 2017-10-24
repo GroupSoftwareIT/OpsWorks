cae5db5c1165e0f67c00f71e1b7c1c1ab59fcd28
Luiz A. Amelotti <luiz.amelotti@groupsoftware.com.br>
2017-10-24 15:54:45 -0200

    Ajustes deploy app

--- tomcat8/recipes/deploy.rb
+++ tomcat8/recipes/deploy.rb
@@ -5,9 +5,9 @@ Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' ******
 Chef::Log.info("********** The app's deploy location is '#{app['attributes']['document_root']}' **********")
 
 remote_file "/opt/tomcat8/webapps/#{app['shortname']}.war" do
-  source '#{app['app_source']['url']}'
-  owner 'root'
-  mode '0644'
+  source "#{app['app_source']['url']}"
+  owner "root"
+  mode "0644"
   action :create
 end
 
