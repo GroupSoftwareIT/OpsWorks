7dd7879198fc79e3053bb6c3463625c69de67054
Luiz A. Amelotti <luiz@amelotti.com>
2018-12-10 11:31:24 -0200

    correcao de credenciais

--- gdefault/attributes/default.rb
+++ gdefault/attributes/default.rb
@@ -1,2 +1,2 @@
-default['gdefault']['ak'] = "AKIAJPBS4QIXE27VEJXA"
-default['gdefault']['sk'] = "/vgE8ZmOIysnNTvncvFa6RJmahcMhXPUhT0f5xD4"
+default['gdefault']['ak'] = ""
+default['gdefault']['sk'] = ""
